21c0445fb7267796860e01213befc91cc8f5cc0b
Andrew <feshykandrew.work@gmail.com>
2023-02-05 15:26:08 +0100

    Update Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [Controller::class, 'show']);
+Route::get('/', function () {
+    return view('welcome');
+});
